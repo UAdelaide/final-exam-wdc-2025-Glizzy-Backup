b64a09acf2155e8bd0eff61af6589e9f4249f0a6
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 05:14:59 +0000

    [automatic commit] 2025-06-21 05:14:59 UTC 101.177.64.117

--- part1/public/javascripts/javascript.js
+++ part1/public/javascripts/javascript.js
@@ -18,7 +18,7 @@ createApp({
                 this.visible = true;
                 this.GetDog();
             }else{
-                document.getElementById("reveal").value = "Oops! Give it another, say... "+counter+" tries.";
+                document.getElementById("reveal").innerHTML = "Oops! Give it another, say... "+counter+" tries.";
                 counter++;
             }
         },
