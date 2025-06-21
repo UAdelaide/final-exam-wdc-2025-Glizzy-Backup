ea245356828034d133765bff583ec1205f7e721f
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 07:01:02 +0000

    [automatic commit] 2025-06-21 07:01:02 UTC 101.177.64.117

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const session = require('express-session');
-const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -8,7 +7,6 @@ const app = express();
 // Middleware
 app.use(express.urlencoded({ extended: true }));// Parses HTML form data
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
   secret: 'session-key-for-testing',
   resave: false,
--- part2/routes/index.js
+++ part2/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const path = require('path');
 
 router.get('/', async (req, res) => {
     if (req.session.user) {
