38db0ec4f3397f7d87b99fb49f03637ee7f0b18c
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 06:56:37 +0000

    [automatic commit] 2025-06-21 06:56:37 UTC 101.177.64.117

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -3,18 +3,17 @@ const router = express.Router();
 const db = require('../models/db');
 
 router.get('/', async (req, res) => {
-        if (req.session.user) {
-            const role = req.session.user.role;
+    if (req.session.user) {
+        const role = req.session.user.role;
 
-            if (role === 'owner') {
-                return res.redirect('/owner-dashboard');
-            } else if (role === 'walker') {
-                return res.redirect('/walker-dashboard');
-            }
+        if (role === 'owner') {
+            return res.redirect('/owner-dashboard');
+        } else if (role === 'walker') {
+            return res.redirect('/walker-dashboard');
         }
+    }
 
-        res.sendFile(path.join(__dirname, 'public/login.html'));
-    });
+    res.sendFile(path.join(__dirname, 'public/login.html'));
 });
 
 module.exports = router;
