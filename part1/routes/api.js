25c03c819d31559dcb206dc44803ff106b2d5043
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 04:20:07 +0000

    [automatic commit] 2025-06-21 04:20:07 UTC 101.177.64.117

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -31,7 +31,7 @@ router.get('/walkers/summary', async function (req, res, next) {
     try {
         const pool = await promisedPool;
         // Try to fetch dogs from db.
-        const [dogs] = await pool.query('SELECT name, size FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;');
+        const [walkers] = await pool.query('SELECT name, size FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;');
         res.json({ dogs });
     } catch (e) {
         res.status(500).json({ message: 'Failed to fetch dogs.', error: e.message });
