cb5aba017c4f2443484af01e20b68c92437940a5
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 06:40:48 +0000

    [automatic commit] 2025-06-21 06:40:48 UTC 101.177.64.117

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -36,8 +36,9 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
-// POST login (dummy version)
+// POST Login ------------------------------
 router.post('/login', async (req, res) => {
+  // Get email and password fields from form
   const { email, password } = req.body;
   try {
     const [rows] = await db.query(`
