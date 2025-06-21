600e23264cae6da3edca4015c10b5969f894c60a
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 03:20:29 +0000

    [automatic commit] 2025-06-21 03:20:29 UTC 101.177.64.117

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -5,7 +5,8 @@ var pool = require('../db');
 /* GET home page. */
 router.get('/dogs', async function(req, res, next) {
     try{
-        let dogs = await pool.query('SELECT name, size FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;')
+        let dogs = await pool.query('SELECT name, size FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;');
+        res.json()
     }
 });
 
