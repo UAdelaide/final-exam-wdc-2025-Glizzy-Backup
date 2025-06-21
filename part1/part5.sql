cab4e421966f03a33d151a4bbcf42258480b5e02
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 03:44:15 +0000

    [automatic commit] 2025-06-21 03:44:15 UTC 101.177.64.117

--- part1/part5.sql
+++ part1/part5.sql
@@ -3,13 +3,11 @@ INSERT INTO Users (username, email, password_hash, role) VALUES ('bobwalker', 'b
 INSERT INTO Users (username, email, password_hash, role) VALUES ('carol123', 'carol@example.com', 'hashed789', 'owner');
 INSERT INTO Users (username, email, password_hash, role) VALUES ('glizzy-g', 'gliccy.g@example.com', 'hashed50', 'owner');
 INSERT INTO Users (username, email, password_hash, role) VALUES ('busy-g', 'busy.g@example.com', 'hashed150', 'walker');
-
 INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE Users.username = 'alice123'), 'Max', 'medium');
 INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE Users.username = 'carol123'), 'Bella', 'small');
 INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE Users.username = 'carol123'), 'Madi', 'medium');
 INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE Users.username = 'glizzy-g'), 'Pumpkin', 'medium');
 INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE Users.username = 'alice123'), 'Snuggle Buns Lovey Dove', 'large');
-
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location) VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands');
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted');
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location) VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Madi'), '2025-06-10 08:00:00', 30, 'Thorndon Park');
