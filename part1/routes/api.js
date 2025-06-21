92d40ce8c8c3398c9c2feff45e03fa1a8745c26c
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 04:04:35 +0000

    [automatic commit] 2025-06-21 04:04:35 UTC 101.177.64.117

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -14,4 +14,28 @@ router.get('/dogs', async function (req, res, next) {
     }
 });
 
+/* GET dogs. */
+router.get('/dogs', async function (req, res, next) {
+    try {
+        const pool = await promisedPool;
+        // Try to fetch dogs from db.
+        const [dogs] = await pool.query('SELECT name, size FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;');
+        res.json({ dogs });
+    } catch (e) {
+        res.status(500).json({ message: 'Failed to fetch dogs.', error: e.message });
+    }
+});
+
+/* GET dogs. */
+router.get('/dogs', async function (req, res, next) {
+    try {
+        const pool = await promisedPool;
+        // Try to fetch dogs from db.
+        const [dogs] = await pool.query('SELECT name, size FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;');
+        res.json({ dogs });
+    } catch (e) {
+        res.status(500).json({ message: 'Failed to fetch dogs.', error: e.message });
+    }
+});
+
 module.exports = router;
