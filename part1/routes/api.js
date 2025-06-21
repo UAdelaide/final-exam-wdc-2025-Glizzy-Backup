d80cb2b21addabf6e0a165cd452efa87e593a4c0
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 04:47:42 +0000

    [automatic commit] 2025-06-21 04:47:42 UTC 101.177.64.117

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -34,7 +34,7 @@ router.get('/walkers/summary', async function (req, res, next) {
         const [walkers] = await pool.query('SELECT Users.username, COUNT(WalkRatings.rating_id) AS total_ratings, ROUND(AVG(WalkRatings.rating), 1) AS average_rating, COUNT(DISTINCT CASE WHEN WalkRequests.status = \'completed\' THEN WalkRequests.request_id END) AS completed_walks FROM Users INNER JOIN WalkRatings ON Users.user_id = WalkRatings.walker_id INNER JOIN WalkRequests ON WalkRatings.request_id = WalkRequests.request_id AND WalkRequests.status = \'completed\' WHERE Users.role = \'walker\' GROUP BY Users.user_id, Users.username;');
         res.json(walkers.map(walker => ({walker_username: walker.username, total_ratings: walker.total_ratings, average_rating: walker.average_rating, completed_walks: walker.completed_walks})));
     } catch (e) {
-        res.status(500).json({ message: 'Failed to fetch dogs.', error: e.message });
+        res.status(500).json({ message: 'Failed to fetch walkers summary.', error: e.message });
     }
 });
 
