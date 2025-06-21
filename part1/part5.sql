4055d91d872b6337794ce39ec0a2384489b54a7e
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 02:00:41 +0000

    [automatic commit] 2025-06-21 02:00:41 UTC 101.177.64.117

--- part1/part5.sql
+++ part1/part5.sql
@@ -13,5 +13,5 @@ INSERT INTO Dogs VALUES ((SELECT user_id FROM Users WHERE Users.username = 'alic
 INSERT INTO WalkRequests VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands')
 INSERT INTO WalkRequests VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave')
 INSERT INTO WalkRequests VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max'), '2025-06-10 08:00:00', 30, 'Thorndon Park')
-INSERT INTO WalkRequests VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max'), '2025-06-10 08:00:00', 30, 'Bowsers Castle')
+INSERT INTO WalkRequests VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max'), '2025-06-10 08:00:00', 30, 'Bowser''s Castle')
 INSERT INTO WalkRequests VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands')
