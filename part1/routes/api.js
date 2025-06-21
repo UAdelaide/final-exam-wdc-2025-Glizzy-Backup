9f505135fb8bd00f6dffb9dc23d7a133dd7c41d3
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 04:29:58 +0000

    [automatic commit] 2025-06-21 04:29:58 UTC 101.177.64.117

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -31,8 +31,7 @@ router.get('/walkers/summary', async function (req, res, next) {
     try {
         const pool = await promisedPool;
         // Try to fetch dogs from db.
-        const [walkers] = await pool.query('SELECT username, COUNT(rating_id) AS total_ratings,
-  ROUND(AVG(r.rating), 1) AS average_rating,
+        const [walkers] = await pool.query('SELECT username, COUNT(rating_id), ROUND(AVG(rating), 1) AS average_rating,
   COUNT(DISTINCT CASE WHEN wr.status = 'completed' THEN wr.request_id END) AS completed_walks
 FROM Users u
 LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
