45975cbc699d09681d2d60d8daadf00ea723f430
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 02:48:54 +0000

    [automatic commit] 2025-06-21 02:48:54 UTC 101.177.64.117

--- part1/app.js
+++ part1/app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api.js');
@@ -15,44 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-let db;
-
-(async () => {
-  try {
-    // Connect to MySQL without specifying a database
-    const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '' // Set your MySQL root password
-    });
-
-    // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
-    await connection.end();
-
-    // Now connect to the created database
-    db = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'DogWalkService'
-    });
-
-    // Insert dogwalks.sql data. Note that this will reset the database every time
-    // (suitable as this is for testing purposes)
-    await db.execute(
-        fs.readFileSync('./dogwalks.sql', 'utf-8')
-    );
-
-    // Insert test data
-    await db.execute(
-        fs.readFileSync('./part5.sql', 'utf-8')
-    );
-  } catch (err) {
-    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
-  }
-})();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- part1/db.js
+++ part1/db.js
@@ -1,2 +1,40 @@
 var mysql = require('mysql2/promise');
 var fs = require('fs');
+
+let db;
+
+(async () => {
+  try {
+    // Connect to MySQL without specifying a database
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '' // Set your MySQL root password
+    });
+
+    // Create the database if it doesn't exist
+    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
+    await connection.end();
+
+    // Now connect to the created database
+    db = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'DogWalkService'
+    });
+
+    // Insert dogwalks.sql data. Note that this will reset the database every time
+    // (suitable as this is for testing purposes)
+    await db.execute(
+        fs.readFileSync('./dogwalks.sql', 'utf-8')
+    );
+
+    // Insert test data
+    await db.execute(
+        fs.readFileSync('./part5.sql', 'utf-8')
+    );
+  } catch (err) {
+    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
+  }
+})();
