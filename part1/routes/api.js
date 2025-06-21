366d5ea21af0a04e8ac6e3c3a53444d11df00127
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 04:17:55 +0000

    [automatic commit] 2025-06-21 04:17:55 UTC 101.177.64.117

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -20,7 +20,7 @@ router.get('/walkrequests/open', async function (req, res, next) {
         const pool = await promisedPool;
         // Try to fetch dogs from db.
         const [requests] = await pool.query('SELECT request_id, name, requested_time, duration_minutes, location, username FROM WalkRequests INNER JOIN (Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id) ON WalkRequests.dog_id = Dogs.dog_id WHERE status=\'open\';');
-        res.json({ request_id: requests.request_id, dog_name: requests.dog_name, requested_time: requests.requested_time, duration_minutes: requests.duration_minutes, location: requests.loaction, owner_username: requests.username });
+        res.json(requests.map(request_id: requests.request_id, dog_name: requests.dog_name, requested_time: requests.requested_time, duration_minutes: requests.duration_minutes, location: requests.loaction, owner_username: requests.username });
     } catch (e) {
         res.status(500).json({ message: 'Failed to fetch open walk requests.', error: e.message });
     }
