235c99ee74edbe1382166a2da2db36611fbd4740
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 03:33:47 +0000

    [automatic commit] 2025-06-21 03:33:46 UTC 101.177.64.117

--- part1/db.js
+++ part1/db.js
@@ -14,9 +14,7 @@ let pool;
 
     // Create dogwalks.sql db. Note that this will reset the database every time
     // (suitable as this is for testing purposes)
-    fs.readFile('./dogwalks.sql', 'utf-8').split('/n').forEach(query=>{
-        await connection.query(query);
-    })
+    await connection.query(fs.readFileSync('./dogwalks.sql', 'utf-8'));
     await connection.end();
 
     // Now connect to the created database
