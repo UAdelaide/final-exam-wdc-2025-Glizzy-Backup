9d453b90f822588ba606183b9a252a8c8b370b7e
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 02:06:19 +0000

    [automatic commit] 2025-06-21 02:06:19 UTC 101.177.64.117

--- part1/part5.sql
+++ part1/part5.sql
@@ -1,16 +1,16 @@
-INSERT INTO Users VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner')
-INSERT INTO Users VALUES ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
-INSERT INTO Users VALUES ('carol123', 'carol@example.com', 'hashed789', 'owner')
-INSERT INTO Users VALUES ('glizzy-g', 'gliccy.g@example.com', 'hashed50', 'owner')
-INSERT INTO Users VALUES ('busy-g', 'busy.g@example.com', 'hashed150', 'walker')
+INSERT INTO Users VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
+INSERT INTO Users VALUES ('bobwalker', 'bob@example.com', 'hashed456', 'walker');
+INSERT INTO Users VALUES ('carol123', 'carol@example.com', 'hashed789', 'owner');
+INSERT INTO Users VALUES ('glizzy-g', 'gliccy.g@example.com', 'hashed50', 'owner');
+INSERT INTO Users VALUES ('busy-g', 'busy.g@example.com', 'hashed150', 'walker');
 
-INSERT INTO Dogs VALUES ((SELECT user_id FROM Users WHERE Users.username = 'alice123'), 'Max', 'medium')
-INSERT INTO Dogs VALUES ((SELECT user_id FROM Users WHERE Users.username = 'carol123'), 'Bella', 'small')
-INSERT INTO Dogs VALUES ((SELECT user_id FROM Users WHERE Users.username = 'carol123'), 'Madi', 'medium')
-INSERT INTO Dogs VALUES ((SELECT user_id FROM Users WHERE Users.username = 'glizzy-g'), 'Pumpkin', 'medium')
-INSERT INTO Dogs VALUES ((SELECT user_id FROM Users WHERE Users.username = 'alice123'), 'Snuggle Buns Lovey Dove', 'large')
+INSERT INTO Dogs VALUES ((SELECT user_id FROM Users WHERE Users.username = 'alice123'), 'Max', 'medium');
+INSERT INTO Dogs VALUES ((SELECT user_id FROM Users WHERE Users.username = 'carol123'), 'Bella', 'small');
+INSERT INTO Dogs VALUES ((SELECT user_id FROM Users WHERE Users.username = 'carol123'), 'Madi', 'medium');
+INSERT INTO Dogs VALUES ((SELECT user_id FROM Users WHERE Users.username = 'glizzy-g'), 'Pumpkin', 'medium');
+INSERT INTO Dogs VALUES ((SELECT user_id FROM Users WHERE Users.username = 'alice123'), 'Snuggle Buns Lovey Dove', 'large');
 
-INSERT INTO WalkRequests VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands')
+INSERT INTO WalkRequests VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands');
 INSERT INTO WalkRequests VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave')
 INSERT INTO WalkRequests VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Madi'), '2025-06-10 08:00:00', 30, 'Thorndon Park')
 INSERT INTO WalkRequests VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Pumpkin'), '2025-06-10 08:00:00', 30, 'Bowser''s Castle')
