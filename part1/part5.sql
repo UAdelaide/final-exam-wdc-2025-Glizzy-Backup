efda7f1f9ef1b5834c373e12848f43e225d25ac1
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 04:43:54 +0000

    [automatic commit] 2025-06-21 04:43:54 UTC 101.177.64.117

--- part1/part5.sql
+++ part1/part5.sql
@@ -12,9 +12,9 @@ INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'completed');
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted');
-INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location) VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Madi'), '2025-06-10 08:00:00', 30, 'Thorndon Park');
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Madi'), '2025-06-10 08:00:00', 30, 'Thorndon Park', 'completed');
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location) VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Pumpkin'), '2025-06-10 08:00:00', 30, 'Bowser''s Castle');
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location) VALUES ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Snuggle Buns Lovey Dove'), '2025-06-10 08:00:00', 30, 'Narnia');
 
 INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES (1, 2, 1, 5, '');
-INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES (1, 2, 1, 3, '');
+INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES (3, 2, 1, 3, '');
