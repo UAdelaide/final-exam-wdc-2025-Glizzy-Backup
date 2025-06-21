2f054ba2782c8031049ee6967d22d9a8f176843c
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 04:03:38 +0000

    [automatic commit] 2025-06-21 04:03:38 UTC 101.177.64.117

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -1,19 +1,17 @@
 var express = require('express');
 var router = express.Router();
-const promisedPool = require('../db');
+const poolPromise = require('../db');
 
 /* GET dogs. */
 router.get('/dogs', async function (req, res, next) {
     try {
-        const pool = await promisedPool;
+        const pool = await poolPromise;
         // Try to fetch dogs from db.
         const [dogs] = await pool.query('SELECT name, size FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;');
         res.json({ dogs });
     } catch (e) {
-    console.error('DB Error:', e);
-    res.status(500).json({ message: 'Failed to fetch dogs.', error: e.message });
-}
-
+        res.status(500).json({ message: 'Failed to fetch dogs.', error: e.message });
+    }
 });
 
 module.exports = router;
