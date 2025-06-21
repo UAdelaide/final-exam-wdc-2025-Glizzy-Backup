7057b1e78fa79bc54e1122d317e82bb36430db20
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 06:57:53 +0000

    [automatic commit] 2025-06-21 06:57:53 UTC 101.177.64.117

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -6,12 +6,11 @@ router.get('/', async (req, res) => {
     if (req.session.user) {
         const role = req.session.user.role;
 
-        if (role === 'owner') {            res.sendFile(path.join(__dirname, '../public/owner-dashboard.html'));
-        
-            res.redirect('/owner-dashboard');            res.sendFile(path.join(__dirname, '../public/owner-dashboard.html'));
+        if (role === 'owner') {
+            res.sendFile(path.join(__dirname, '../public/owner-dashboard.html'));
 
         } else if (role === 'walker') {
-            return res.redirect('/walker-dashboard');
+            res.sendFile(path.join(__dirname, '../public/walker-dashboard.html'));
         }
     }
 
