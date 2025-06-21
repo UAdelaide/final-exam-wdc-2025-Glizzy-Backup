e3f3346c4a0dd3c1d8a234fa514e98702654c71c
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 02:55:12 +0000

    [automatic commit] 2025-06-21 02:55:12 UTC 101.177.64.117

--- part1/db.js
+++ part1/db.js
@@ -17,7 +17,7 @@ let db;
     await connection.end();
 
     // Now connect to the created database
-    db = await mysql.createConnection({
+    db = await mysql.createPool({
       host: 'localhost',
       user: 'root',
       password: '',
