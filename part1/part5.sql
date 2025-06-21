a4f2f5127c1cc7ee7c1f46d30b09d60472540654
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 01:47:15 +0000

    [automatic commit] 2025-06-21 01:47:15 UTC 101.177.64.117

--- part1/part5.sql
+++ part1/part5.sql
@@ -1,4 +1,5 @@
 INSERT INTO Users VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner')
 INSERT INTO Users VALUES ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
 INSERT INTO Users VALUES ('carol123', 'carol@example.com', 'hashed789', 'owner')
-INSERT INTO Users VALUES ('glizzy-g', 'gliccy.g@example.com', 'hashed', 'owner')
+INSERT INTO Users VALUES ('glizzy-g', 'gliccy.g@example.com', 'hashed150', 'owner')
+INSERT INTO Users VALUES ('carol123', 'carol@example.com', 'hashed789', 'owner')
