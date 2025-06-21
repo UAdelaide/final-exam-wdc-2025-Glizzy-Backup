0c230f2786f440ec855a611764567ecebd4f6ddd
a1927208 (Backup Acc.) <bloople3@outlook.com>
2025-06-21 03:22:42 +0000

    [automatic commit] 2025-06-21 03:22:42 UTC 101.177.64.117

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var pool = require('../db');
 
 /* GET home page. */
-router.get('/dogs', async function(req, res, next) {
-    try{
+router.get('/dogs', async function (req, res, next) {
+    try {
         let dogs = await pool.query('SELECT name, size FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;');
         res.json({ dogs });
-    } catch(e){
-        res.status(500).json({'Failed to fetch dogs.'});
+    } catch (e) {
+        res.status(500).json({ message: 'Failed to fetch dogs.' });
     }
 });
 
